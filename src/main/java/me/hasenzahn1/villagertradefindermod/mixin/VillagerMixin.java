c057c5414f48af86b4adb1aba0c695b2a1bf86a7
BuildTools <unconfigured@null.spigotmc.org>
2022-01-31 16:25:48 +0100

    Remove Mixins

--- src/main/java/me/hasenzahn1/villagertradefindermod/mixin/VillagerMixin.java
+++
@@ -1,8 +0,0 @@
-package me.hasenzahn1.villagertradefindermod.mixin;
-
-import net.minecraft.client.gui.screen.TitleScreen;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(TitleScreen.class)
-public class VillagerMixin {
-}
